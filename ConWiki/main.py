8ee5650da2f480f417d7ee6fa62798d34300b54e
Ralph Bayer <RalphBayer@protonmail.com>
2020-05-25 18:13:39 +0100

    Added a flask functions for json requests

--- ConWiki/main.py
+++ ConWiki/main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response, jsonify
+from article_manager import *
 
 app = Flask(__name__)
+
+# ---------------------------- Functions for Flask ---------------------------- #
 
 @app.route("/")
 def main():
@@ -21,6 +24,14 @@ def create():
 @app.route("/help.html")
 def help():
     return render_template("help.html")
+
+@app.route("/create.html/post_json", methods=['POST'])
+def json():
+    req = request.get_json()
+    create_article(req)
+    return make_response(jsonify({"message":"JSON received"}), 200)
+
+# ---------------------------- Starts the flask server ---------------------------- #
 
 if __name__ == "__main__":
     app.run()
